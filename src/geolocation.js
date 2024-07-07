8ae63362c064963222281265d09973ac606cf725
Carlos Vializ <cvializ@vt.edu>
2024-07-07 10:33:00 -0400

    fixed it I hope

--- src/geolocation.js
+++ src/geolocation.js
@@ -1,42 +1,44 @@
 // import { createObservable } from "./observables/createObservable.js";
 
-const { Observable } = window.rxjs;
+const { Subject } = window.rxjs;
 
 const createObservable = subscriber => new Observable(subscriber);
 
 export const watchPosition = () => {
-    return createObservable(({ next, error, complete }) => {
-        const id = window.navigator.geolocation.watchPosition(
-            ({
-                timestamp,
-                coords,
-            }) => {
-                next({
-                    id,
-                    timestamp,
-                    accuracy: coords.accuracy,
-                    latitude: coords.latitude,
-                    longitude: coords.longitude,
-                    altitude: coords.altitude,
-                    altitudeAccuracy: coords.altitudeAccuracy,
-                    heading: coords.heading,
-                    speed: coords.speed,
-                });
-            },
-            (e) => {
-                // error(e.message)
-            },
-            {
-                enableHighAccuracy: true,
-                maximumAge: 10,
-                timeout: 27000,
-            }
-        );
+    const subject$ = new Subject();
 
-        return () => {
-            window.navigator.geolocation.clearWatch(id);
-        };
-    });
+    const id = window.navigator.geolocation.watchPosition(
+        ({
+            timestamp,
+            coords,
+        }) => {
+            subject$.next({
+                id,
+                timestamp,
+                accuracy: coords.accuracy,
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+                altitude: coords.altitude,
+                altitudeAccuracy: coords.altitudeAccuracy,
+                heading: coords.heading,
+                speed: coords.speed,
+            });
+        },
+        (e) => {
+            subject$.error(e.message)
+        },
+        {
+            enableHighAccuracy: true,
+            maximumAge: 10,
+            timeout: 27000,
+        }
+    );
+
+    return subject$.asObservable();
+
+    // return () => {
+    //     window.navigator.geolocation.clearWatch(id);
+    // };
 };
 
 export const clearWatchPosition = (id) => navigator.geolocation.clearWatch(id);
